efcfce0a749614359ddc570b9633474a792a72d0
William Meyer <valodier@icloud.com>
2022-08-07 14:00:37 -0400

    Adds solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = (string) => {
+  let i = string.length - 1;
+  reverse = "";
 
+  while (i >= 0) {
+    reverse += string[i];
+    i--;
+  }
+  return reverse;
 };
 
 // Do not edit below this line
