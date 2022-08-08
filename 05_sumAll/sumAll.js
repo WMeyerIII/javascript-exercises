da041de72d7a2979b6f4084dd3b1beeacac15f69
William Meyer <valodier@icloud.com>
2022-08-08 08:40:32 -0400

    Adds 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  let max = Math.max(num1, num2);
+  let min = Math.min(num1, num2);
+
+  let total = 0;
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
