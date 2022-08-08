10e226c0e75a70869153421d78630f37bd99f4b5
William Meyer <valodier@icloud.com>
2022-08-08 09:28:30 -0400

    Adds 07_tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  celcius = (temperature - 32) * (5 / 9);
+  return Number(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  fahrenheit = temperature * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
