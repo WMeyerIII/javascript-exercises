170de58f3f0c587357a7190bcbe430b72d1ce7ad
William Meyer <valodier@icloud.com>
2022-08-08 11:05:47 -0400

    Adds 09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  lowerNoPunctuation = string.toLowerCase().replace(/[^a-z]/g, "");
 
+  reverse = lowerNoPunctuation.split("").reverse().join("");
+  return lowerNoPunctuation == reverse;
 };
 
 // Do not edit below this line
