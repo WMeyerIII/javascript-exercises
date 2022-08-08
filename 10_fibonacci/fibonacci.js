1e024ad54647de01fbfe369b023eb0cedb4b588a
William Meyer <valodier@icloud.com>
2022-08-08 11:23:00 -0400

    Adds 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  array = [0, 1];
 
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 1; i < number; i++) {
+    next = array[0] + array[1];
+    array[0] = array[1];
+    array[1] = next;
+  }
+  return array[1];
 };
 
 // Do not edit below this line
