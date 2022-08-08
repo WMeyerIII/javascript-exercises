ae09fd7d659b8d24c24778af2ae4b772ce0da74c
William Meyer <valodier@icloud.com>
2022-08-08 07:37:10 -0400

    Adds solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  for (const arg of args) {
+    if (array.includes(arg)) {
+      argIndex = array.indexOf(arg);
+      array.splice(argIndex, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
